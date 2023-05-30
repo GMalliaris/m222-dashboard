eceb94d5449563e607007d44e10352804175d0ac
gmalliaris <georgemalliaris8@gmail.com>
2023-05-30 21:36:03 +0300

    some more cleaning

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import logo from "../assets/Amazon-Logo.png"
 import { HiChevronUp, HiChevronDown } from "react-icons/hi";
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 type ApplicationLogoProps = {
     isToggleOn: boolean,
@@ -11,12 +11,14 @@ type ApplicationLogoProps = {
 const ApplicationLogo = ({ isToggleOn, setToggle }: ApplicationLogoProps) => {
     return (
         <div className={`flex sm:block ${isToggleOn ? "mb-10" : ""} sm:mb-20`}>
-            <h1 className="text-4xl font-bold align-middle">
-                <p>
-                    Amazon
-                    <img className="inline-block ml-4" src={logo} height={32} width={128} alt="amazon logo" />
-                </p>
-                <p>Reviews Analysis</p>
+            <h1 className="text-4xl font-bold align-middle cursor-pointer">
+                <NavLink to="/">
+                    <p>
+                        Amazon
+                        <img className="inline-block ml-4" src={logo} height={32} width={128} alt="amazon logo" />
+                    </p>
+                    <p>Reviews Analysis</p>
+                </NavLink>
             </h1>
             <div className="flex sm:hidden flex-grow justify-end items-center text-4xl" onClick={setToggle}>
                 { isToggleOn ?  <HiChevronUp className="cursor-pointer" /> : <HiChevronDown className="cursor-pointer" />}
@@ -25,13 +27,29 @@ const ApplicationLogo = ({ isToggleOn, setToggle }: ApplicationLogoProps) => {
     );
 }
 
+type NavigationBarLinkProps = {
+    location: string;
+    label: string;
+};
+
+const NavigationBarLink = (props: NavigationBarLinkProps) => {
+    return (
+        <NavLink to={props.location} className={({isActive}) => {
+            const standardClasses = "block pl-2 transition-colors hover:bg-orange-400 hover:rounded-md border-black hover:border-r-[6px]";
+            const activeClass = isActive ? " border-r-[6px] bg-orange-400 rounded-md" : "";
+            return standardClasses + activeClass;
+            }}>
+            { props.label }
+        </NavLink>
+    );
+}
+
 const NavigationBar = () => {
 
     const [optionsVisible, setOptionsVisible] = useState(false);
     const [highscoresVisible, setHighscoresVisible] = useState(false);
 
-
-    return <div className="bg-orange-300 py-8 px-12">
+    return <div className="bg-orange-300 py-8 px-10 max-h-[100vh]">
         <ApplicationLogo isToggleOn={optionsVisible} setToggle={() => setOptionsVisible((prevState) => !prevState)}/>
 
         <div>
@@ -41,21 +59,21 @@ const NavigationBar = () => {
             </h3>
             {optionsVisible && <ul className="text-2xl font-medium my-4 ml-4">
                 <li className="mb-1">
-                    <Link className="block pl-2 transition-colors hover:bg-orange-400 hover:rounded-md" to="/">Dashboard</Link>
+                    <NavigationBarLink label="Dashboard" location="/"/>
                 </li>
                 <li className="mb-1">
-                    <Link className="block pl-2 transition-colors hover:bg-orange-400 hover:rounded-md" to="/products">Products History</Link>
+                    <NavigationBarLink label="Products History" location="/products"/>
                 </li>
                 <li className="pl-2 mb-1 flex justify-between items-center cursor-pointer" onClick={() => setHighscoresVisible((prevState) => !prevState)} >
                     Highscores
                     { highscoresVisible ? <HiChevronUp /> : <HiChevronDown />}
                 </li>
                 { highscoresVisible && <ul className="text-lg my-2 ml-3">
-                    <li>
-                        <Link className="block pl-2 transition-colors hover:bg-orange-400 hover:rounded-md" to="/best"> Best Reviews</Link>
+                    <li className="mb-1">
+                        <NavigationBarLink label="Best Reviews" location="/best"/>
                     </li>
                     <li>
-                        <Link className="block pl-2 transition-colors hover:bg-orange-400 hover:rounded-md" to="/worst"> Worst Reviews</Link>
+                        <NavigationBarLink label="Worst Reviews" location="/worst"/>
                     </li>
                 </ul> }
             </ul>}
